1cad9e9042941fae4a6b949c3217d827fe0940da
Anastasiagumenuk9 <44962434+Anastasiagumenuk9@users.noreply.github.com>
2019-12-05 21:38:51 +0200

    *

---
+++ Application/CARS/ClientApp/src/containers/Car/CarList/CarList.js
@@ -0,0 +1,70 @@
+﻿import React, { Component } from 'react';
+import { Table, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Car from '../../../components/CarComponents/Car/Car';
+import { PropTypes } from 'react'
+
+import { connect } from 'react-redux';
+import * as repositoryActions from '../../../store/actions/repositoryActions';
+
+export class CarList extends Component {
+
+    componentDidMount = () => {
+        let url = '/api/car';
+        this.props.onGetData(url, { ...this.props });
+    }
+
+    render() {
+        let cars = [];
+        if (this.props.data && this.props.data.length > 0) {
+            cars = this.props.data.map((car) => {
+                return (
+                    <Car key={car.Id} car={car} {...this.props} />
+                )
+            })
+        }
+
+        return (
+            <Aux>
+                <Row>
+                    <Col mdOffset={10} md={2}>
+                        <Link to='/createCar' >CreateCar</Link>
+                    </Col>
+                </Row>
+                <br />
+                <Row>
+                    <Col md={12}>
+                        <Table responsive striped>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Price</th>
+                                    <th>Available</th>
+                                    <th>Update</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cars}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Aux>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        data: state.data
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onGetData: (url, props) => dispatch(repositoryActions.getData(url, props))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CarList);
--- Application/CARS/ClientApp/src/index.js
+++ Application/CARS/ClientApp/src/index.js
@@ -6,6 +6,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import repositoryReducer from './store/reducers/repositoryReducer';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(repositoryReducer, applyMiddleware(thunk));
+
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
