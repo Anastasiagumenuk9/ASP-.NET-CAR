7c86d9c2fa40784de62c0f4993ab77082c499829
Anastasiagumenuk9 <44962434+Anastasiagumenuk9@users.noreply.github.com>
2019-12-05 21:38:20 +0200

    *

--- Application/CARS/ClientApp/src/App.js
+++ Application/CARS/ClientApp/src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Car } from './components/Car';
 import { MyAccount } from './components/MyAccount';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+
+import {CarList} from './containers/Car/CarList/CarList';
+import axios from 'axios';
 
 export default class App extends Component {
   displayName = App.name
@@ -14,10 +16,10 @@ export default class App extends Component {
     return (
       <Layout>
             <Route exact path='/' component={Home} />
-            <Route path='/car' component={Car} />
             <Route path='/myaccount' component={MyAccount} />
             <Route path='/counter' component={Counter} />
             <Route path='/fetchdata' component={FetchData} />
+            <Route path='/carlist' component={CarList} />
       </Layout>
     );
   }
