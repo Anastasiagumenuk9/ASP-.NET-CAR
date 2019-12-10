b97a76bf11f6ba9f27ede22696d01777ce1300c5
Anastasiagumenuk9 <44962434+Anastasiagumenuk9@users.noreply.github.com>
2019-12-10 23:11:42 +0200

    *

--- Application/BAL/Services/EmailSender.cs
+++ Application/BAL/Services/EmailSender.cs
@@ -8,11 +8,26 @@ namespace BAL.Services
 {
     public class EmailSender : IEmailSender
     {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var from = "q.u.i.c.k.sender.r.r@gmail.com";
+            var pass = "quicksender123";
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(from, pass);
+            client.EnableSsl = true;
+            var mail = new MailMessage(from, email);
+            mail.Subject = subject;
+            mail.Body = message;
+            mail.IsBodyHtml = true;
+            return client.SendMailAsync(mail);
+        }
 
         public void SendEmail(string email, string subject, string message)
         {
-            var from = "Car@gmail.com";
-            var pass = "C123";
+            var from = "q.u.i.c.k.sender.r.r@gmail.com";
+            var pass = "quicksender123";
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
--- Application/BAL/Services/Interfaces/IEmailSender.cs
+++ Application/BAL/Services/Interfaces/IEmailSender.cs
@@ -7,6 +7,7 @@ namespace BAL.Services
 {
    public interface IEmailSender
     {
+        Task SendEmailAsync(string email, string subject, string message);
         void SendEmail(string email, string subject, string message);
     }
 }
